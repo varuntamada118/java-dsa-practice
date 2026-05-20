4394044a1d8d9fa689500583353a0a3b3a530c26
Varun Tamada <varuntamada118@gmail.com>
2026-05-20 11:05:37 +0530

    rename the file

--- Basics/hollow_box.java
+++ Basics/hollow_box.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-class hallow_box {
+class hollow_box {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
