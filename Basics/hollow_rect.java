421699f70c945318e55afeca3040b7fd5f5b36c5
Varun Tamada <varuntamada118@gmail.com>
2026-05-22 18:17:44 +0530

    Rename hollow_box to hollow_rect
    Rename file and class from hollow_box to hollow_rect to keep filename and class name consistent. Update contains a direct rename with minimal edits (class name change and removal of an extra blank line); program logic unchanged.

--- Basics/hollow_rect.java
+++ Basics/hollow_rect.java
@@ -1,6 +1,5 @@
-
 import java.util.Scanner;
-class hollow_box {
+class hollow_rect {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
